87a98b395b3f0609ef357550c54f00b09ab33708
Kyle Babcock <47157069+babco1kr@users.noreply.github.com>
2020-03-12 14:36:50 -0400

    moved out routes to the own file

--- server.js
+++ server.js
@@ -10,27 +10,11 @@ app.use(express.json());
 // import DB shema
 const db = require("./models");
 
+require("./routes/api-routes.js")(app);
+
 // Setup Sequialize
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-app.post('/createUrl', (req, res) => {
-  db.Url.create({
-    target_url: req.body.target_url,
-    tiny_url: req.body.tiny_url
-    }).then(function(data) {
-    res.json();
-    })
-  })
-}
-
-app.get('/find/:tiny_url', (req, res) => {
-  db.Url.findOne({ where: { tiny_url: req.params.tiny_url }
-  }).then(function(data) {
-    res.json();
-    })
-  })
- }
