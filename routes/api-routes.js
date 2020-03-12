1a28f15e47aeb7aafc03e4e305d64f10c6caf2cc
Kyle Babcock <47157069+babco1kr@users.noreply.github.com>
2020-03-12 14:35:38 -0400

    moved routes to own file

---
+++ routes/api-routes.js
@@ -0,0 +1,22 @@
+const db = require('../models');
+
+module.exports = function(app) {
+
+  app.post('/createUrl', (req, res) => {
+  db.Url.create({
+    target_url: req.body.target_url,
+    tiny_url: req.body.tiny_url
+    }).then(function(data) {
+    res.json();
+    })
+  })
+}
+
+app.get('/find/:tiny_url', (req, res) => {
+  db.Url.findOne({ where: { tiny_url: req.params.tiny_url }
+  }).then(function(data) {
+    res.json();
+    })
+  })
+
+}
